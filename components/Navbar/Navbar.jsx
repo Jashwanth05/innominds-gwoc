221cd3a0e7731e49cde8b720b8fcca3a695543b8
Jashwanth05 <139554684+Jashwanth05@users.noreply.github.com>
2023-12-29 09:59:07 +0530

    Add files via upload

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+
+  return (
+    <div class="navbar">
+    <div class="frame-30">
+    <div class="rectangle-24"></div>
+    <div class="logo">
+    <div class="t">T</div>
+    <div class="t2">T</div>
+      <div class="tutors">Tutors</div>
+      <div class="tarang">Tarang</div>
+    </div>
+    <div class="login">
+      <div class="login2">Sign up</div>
+    </div>
+    <div class="frame-26">
+     
+        <div class="home">About Us<hr/></div>
+     
+      
+        <div class="home">Courses<hr></hr></div>
+
+
+        <div class="home">Feedback<hr></hr></div>
+ 
+  
+        <div class="home">Contact<hr></hr></div>
+
+    </div>
+    <div class="login3">
+      <div class="login2">Search</div>
+    </div>
+    <svg
+      class="vector"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.1527 15.0943H16.0686L15.6844 14.7238C17.0292 13.1595 17.8388 11.1286 17.8388 8.91938C17.8388 3.99314 13.8456 0 8.91938 0C3.99314 0 0 3.99314 0 8.91938C0 13.8456 3.99314 17.8388 8.91938 17.8388C11.1286 17.8388 13.1595 17.0292 14.7238 15.6844L15.0943 16.0686V17.1527L21.9554 24L24 21.9554L17.1527 15.0943ZM8.91938 15.0943C5.50257 15.0943 2.74443 12.3362 2.74443 8.91938C2.74443 5.50257 5.50257 2.74443 8.91938 2.74443C12.3362 2.74443 15.0943 5.50257 15.0943 8.91938C15.0943 12.3362 12.3362 15.0943 8.91938 15.0943Z"
+        fill="white"
+      />
+    </svg>
+  </div>
+  <div class="frame-24">
+  <div class="rectangle-24"></div>
+  <div class="logo">
+    <div class="tutors1">Tutors</div>
+    <div class="tarang1">Tarang</div>
+  </div>
+  <svg
+    class="menu"
+    width="45"
+    height="45"
+    viewBox="0 0 45 45"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_104_614)">
+      <path
+        d="M5.625 33.75H39.375V30H5.625V33.75ZM5.625 24.375H39.375V20.625H5.625V24.375ZM5.625 11.25V15H39.375V11.25H5.625Z"
+        fill="white"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_104_614">
+        <rect width="45" height="45" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+
+  <svg
+    class="vector3"
+    width="48"
+    height="48"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M34.3053 30.1887H32.1372L31.3688 29.4477C34.0583 26.319 35.6775 22.2573 35.6775 17.8388C35.6775 7.98628 27.6912 0 17.8388 0C7.98628 0 0 7.98628 0 17.8388C0 27.6913 7.98628 35.6775 17.8388 35.6775C22.2573 35.6775 26.319 34.0583 29.4477 31.3688L30.1887 32.1372V34.3053L43.9108 48L48 43.9108L34.3053 30.1887ZM17.8388 30.1887C11.0051 30.1887 5.48885 24.6724 5.48885 17.8388C5.48885 11.0051 11.0051 5.48885 17.8388 5.48885C24.6724 5.48885 30.1887 11.0051 30.1887 17.8388C30.1887 24.6724 24.6724 30.1887 17.8388 30.1887Z"
+      fill="white"
+    />
+  </svg>
+</div>
+
+  </div>
+  
+
+
+  )
+}
+
+export default Navbar
