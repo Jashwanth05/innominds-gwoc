8e0a0e55738735144c9c2c297e5288e57a93706c
Jashwanth05 <139554684+Jashwanth05@users.noreply.github.com>
2023-12-30 20:16:19 +0530

    Add files via upload

---
+++ components/homepage/Home.jsx
@@ -0,0 +1,441 @@
+import React from 'react';
+import back from '../../components/photos and videos/back0.png';
+import boyandgirl from '../../components/photos and videos/boy-and-girl0.png';
+import forward from '../../components/photos and videos/forward0.png';
+import instagram from '../../components/photos and videos/instagram-logo0.png';
+import instructor from '../../components/photos and videos/instructor0.png';
+import ioc from '../../components/photos and videos/ioc-png0.png';
+import linkedin from '../../components/photos and videos/linkedin-icon0.png';
+import maths from '../../components/photos and videos/maths0.png';
+import mechanics from '../../components/photos and videos/mechanics0.png';
+import physics from '../../components/photos and videos/physics0.png';
+import student from '../../components/photos and videos/student0.png';
+import svnit from '../../components/photos and videos/svnit0.png';
+import testubes from '../../components/photos and videos/test-tubes-and-flask0.png';
+import twitter from '../../components/photos and videos/twitter1.png';
+import user1 from '../../components/photos and videos/user-10.png';
+import user2 from '../../components/photos and videos/user-20.png';
+import user3 from '../../components/photos and videos/user-30.png';
+import user4 from '../../components/photos and videos/user-40.png';
+import user5 from '../../components/photos and videos/user-50.png';
+import youtube from '../../components/photos and videos/youtube-icon0.png';
+
+import './home.css';
+
+const Home = () => {
+  return (
+<div class="frame-1">
+  <div class="group-3">
+  <img class="boy-and-girl" src={boyandgirl} alt="boyandgirl"/>
+    <div class="frame-3">
+      <div class="learn-with-love">Learn with Love</div>
+      <div class="the-ultimate-guide-to-ace-sde-interviews">
+        <span>
+          <span class="the-ultimate-guide-to-ace-sde-interviews-span">
+            The Ultimate Guide To
+            <br />
+          </span>
+          <span class="the-ultimate-guide-to-ace-sde-interviews-span2">
+            AceSDE
+          </span>
+          <span class="the-ultimate-guide-to-ace-sde-interviews-span3">
+            Interviews.
+          </span>
+        </span>
+      </div>
+    </div>
+    <div class="frame-6">
+      <div class="watch-video">Watch Video</div>
+    </div>
+    <div class="frame-7">
+      <div class="view-course">View Course</div>
+    </div>
+  </div>
+  <div class="group-4">
+    <div class="frame-12">
+      <img class="user-5" src={user5} alt="user5" />
+    </div>
+    <div class="frame-11">
+      <img class="user-4" src={user4} alt="user4" />
+    </div>
+    <div class="frame-10">
+      <img class="user-3" src={user3} alt="user3" />
+    </div>
+    <div class="frame-9">
+      <img class="user-2" src={user2} alt="user2" />
+    </div>
+    <div class="frame-8">
+      <img class="user-1" src={user1} alt="user1" />
+    </div>
+    <div class="_3500-happy-students">
+      <span>
+        <span class="_3500-happy-students-span">
+          3500+
+          <br />
+        </span>
+        <span class="_3500-happy-students-span2">Happy Students</span>
+      </span>
+    </div>
+  </div>
+  <div class="group-6">
+    <div class="rectangle-14"></div>
+    <div class="frame-5">
+      <div class="_960-k-subscribers-on-youtube">
+        <span>
+          <span class="_960-k-subscribers-on-youtube-span">
+            960K+
+            <br />
+          </span>
+          <span class="_960-k-subscribers-on-youtube-span2">
+            Subscribers
+            <br />
+          </span>
+          <span class="_960-k-subscribers-on-youtube-span3">on Youtube</span>
+        </span>
+      </div>
+      <div class="_6900-subscribers-on-twitter">
+        <span>
+          <span class="_6900-subscribers-on-twitter-span">
+            6900+
+            <br />
+          </span>
+          <span class="_6900-subscribers-on-twitter-span2">
+            Subscribers
+            <br />
+          </span>
+          <span class="_6900-subscribers-on-twitter-span3">on Twitter</span>
+        </span>
+      </div>
+      <div class="_69-k-subscribers-on-instagram">
+        <span>
+          <span class="_69-k-subscribers-on-instagram-span">
+            69K+
+            <br />
+          </span>
+          <span class="_69-k-subscribers-on-instagram-span2">
+            Subscribers
+            <br />
+          </span>
+          <span class="_69-k-subscribers-on-instagram-span3">on Instagram</span>
+        </span>
+      </div>
+      <div class="_96-m-subscribers-on-linke-in">
+        <span>
+          <span class="_96-m-subscribers-on-linke-in-span">
+            96M+
+            <br />
+          </span>
+          <span class="_96-m-subscribers-on-linke-in-span2">
+            Subscribers
+            <br />
+          </span>
+          <span class="_96-m-subscribers-on-linke-in-span3">on LinkeIn.</span>
+        </span>
+      </div>
+    </div>
+  </div>
+  <div
+    class="why-tarang-tutors-making-learning-easier-and-more-convenient-for-you"
+  >
+    <span>
+      <span
+        class="why-tarang-tutors-making-learning-easier-and-more-convenient-for-you-span"
+      >
+        Why Tarang Tutors?
+        <br />
+      </span>
+      <span
+        class="why-tarang-tutors-making-learning-easier-and-more-convenient-for-you-span2"
+      >
+        Making learning easier and
+        <br />
+        more convenient for you
+      </span>
+      <span
+        class="why-tarang-tutors-making-learning-easier-and-more-convenient-for-you-span3"
+      >
+        .
+      </span>
+    </span>
+  </div>
+  <div class="rectangle-26"></div>
+
+  <div class="founder-tarang-tutors">
+    <span>
+      <span class="founder-tarang-tutors-span">Founder Tarang Tutors</span>
+      <span class="founder-tarang-tutors-span2"></span>
+    </span>
+  </div>
+  <div class="meet-your-instructor">
+    <span>
+      <span class="meet-your-instructor-span">Meet your</span>
+      <span class="meet-your-instructor-span2">Instructor</span>
+    </span>
+  </div>
+  <div class="rectangle-27"></div>
+  <div class="know-more">Know more</div>
+  <div class="line-5"></div>
+  <div class="text">
+    <br />
+  </div>
+  <svg
+    class="rectangle-28"
+    width="1920"
+    height="831"
+    viewBox="0 0 1920 831"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0 0H1920V831H0V0Z" fill="#111111" />
+  </svg>
+
+  <div class="our-graduates-land-exclusive-opportunities-with-premier-colleges">
+    Our graduates land exclusive opportunities with premier colleges
+  </div>
+  <div class="rectangle-20">
+    <img class="instructor" src={instructor} alt="instructor" />
+  </div>
+  <div class="admissions">
+    ADMISSIONS
+    <br />
+  </div>
+  <div class="get-offers-from-top-collages">
+    Get Offers From
+    <br />
+    Top Collages
+  </div>
+  <div class="line-6"></div>
+  <div class="what-would-you-like-to-learn">
+    <span>
+      <span class="what-would-you-like-to-learn-span">
+        What would you like to
+      </span>
+      <span class="what-would-you-like-to-learn-span2">learn ?</span>
+      <span class="what-would-you-like-to-learn-span3"></span>
+    </span>
+  </div>
+  <div class="join-our-family">Join our family</div>
+  <div class="frame-32">
+    <img class="mechanics" src={mechanics} alt="mechanics" />
+    <div class="mechanics-supra-batch">Mechanics @supra batch</div>
+    <div class="line-7"></div>
+    <div class="_2000">₹2000</div>
+  </div>
+  <div class="frame-33">
+    <div class="inorganic-chemistry-supra-batch">
+      Inorganic chemistry @supra batch
+    </div>
+    <div class="line-72"></div>
+    <div class="_2500">
+      ₹2500
+      <br />
+    </div>
+    <div class="line-62"></div>
+    <img class="ioc-png" src={ioc} alt="ioc" />
+  </div>
+  <div class="frame-34">
+    <div class="what-student-says">What student says</div>
+    <div class="recent-reviews">Recent Reviews</div>
+    <div class="line-63"></div>
+    <img class="back" src={back} alt="back" />
+    <img class="forward" src={forward} alt="forward" />
+    <div class="group-24">
+      <div class="rectangle-29"></div>
+    </div>
+    <div
+      class="studying-at-tarang-tutors-was-a-game-changer-for-me-the-well-structured-courses-doubt-solving-sessions-and-constant-support-from-the-faculty-were-instrumental-in-my-jee-success-i-couldn-t-have-done-it-without-them"
+    >
+      Studying at Tarang Tutors was a game-changer for me. The well-structured
+      courses, doubt-solving sessions, and constant support from the faculty
+      were instrumental in my JEE success. I couldn&#039;t have done it without
+      them.
+    </div>
+    <div class="shivani">Shivani</div>
+    <div class="air-1256">AIR 1256</div>
+    <div class="customer-potraits">
+      <img class="student" src={student} alt="student"  />
+    </div>
+  </div>
+  <div class="group-10">
+    <div class="frame-17"></div>
+    <div class="frame-19"></div>
+  </div>
+  <div class="group-11">
+    <div class="frame-172">
+      <div
+        class="mathematics-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews"
+      >
+        <span>
+          <span
+            class="mathematics-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews-span"
+          >
+            Mathematics
+            <br />
+          </span>
+          <span
+            class="mathematics-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews-span2"
+          >
+            <br />
+            Data structures are the problem-solving pillars of coding. Learn all
+            the foundational knowledge about all of the popular data structure
+            that you need, to be well-versed in interviews.
+          </span>
+        </span>
+      </div>
+    </div>
+    <div class="frame-18">
+      <div
+        class="data-structure-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews"
+      >
+        <span>
+          <span
+            class="data-structure-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews-span"
+          >
+            Data Structure
+            <br />
+          </span>
+          <span
+            class="data-structure-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews-span2"
+          >
+            <br />
+            Data structures are the problem-solving pillars of coding. Learn all
+            the foundational knowledge about all of the popular data structure
+            that you need, to be well-versed in interviews.
+          </span>
+        </span>
+      </div>
+    </div>
+    <div class="frame-192">
+      <div
+        class="mathematics-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews2"
+      >
+        <span>
+          <span
+            class="mathematics-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews-2-span"
+          >
+            Mathematics
+            <br />
+          </span>
+          <span
+            class="mathematics-data-structures-are-the-problem-solving-pillars-of-coding-learn-all-the-foundational-knowledge-about-all-of-the-popular-data-structure-that-you-need-to-be-well-versed-in-interviews-2-span2"
+          >
+            <br />
+            Data structures are the problem-solving pillars of coding. Learn all
+            the foundational knowledge about all of the popular data structure
+            that you need, to be well-versed in interviews.
+          </span>
+        </span>
+      </div>
+    </div>
+  </div>
+  <div
+    class="if-you-would-like-to-keep-up-on-the-latest-posts-and-courses-come-by-and-connect-with-us-on-the-following-links"
+  >
+    If you would like to keep up on the latest posts and courses,come by and
+    connect with us on the following links.
+  </div>
+  <img class="youtube-icon" src={youtube} alt="youtube"/>
+  <img class="linkedin-icon" src={linkedin} alt="linkedin" />
+  <img class="instagram-logo" src={instagram} alt="instagram"  />
+  <div class="youtube">Youtube</div>
+  <div class="instagram">Instagram</div>
+  <div class="twitter">Twitter</div>
+  <div class="linkedin">linkedin</div>
+  <div class="line-8"></div>
+  <div class="line-9"></div>
+  <div class="line-10"></div>
+  <div class="line-11"></div>
+  <div class="group-26">
+    <div class="frame-35">
+      <div class="logo">
+        <div class="t"></div>
+      </div>
+      <div class="menu">MENU</div>
+      <div class="services">SERVICES</div>
+      <div class="line-73"></div>
+      <div
+        class="copyright-2024-sorting-tarang-tutors-technologies-pvt-ltd-all-rights-reserved"
+      >
+        Copyright © 2024 Sorting Tarang Tutors Technologies Pvt Ltd. All Rights
+        Reserved
+      </div>
+      <div class="about-us">
+        About Us
+        <br />
+      </div>
+      <div class="privacy-policy">Privacy Policy</div>
+      <div class="email">Email:</div>
+      <div class="contact">
+        Contact
+        <br />
+      </div>
+      <div class="labs">
+        Labs
+        <br />
+      </div>
+      <div class="courses">Courses</div>
+      <div class="logo-side-bar">
+  
+      </div>
+      <div class="the-ultimate-guide-to-ace-exam">
+        The Ultimate Guide To Ace Exam
+      </div>
+      <div class="terms-of-uses">Terms Of Uses</div>
+      <div class="refund-cancellation-policy">
+        Refund &amp; Cancellation Policy
+      </div>
+      <div class="line-112"></div>
+      <div class="get-in-touch">GET IN TOUCH</div>
+    </div>
+  </div>
+  <img class="twitter2" src={twitter} alt="twitter" />
+  <div class="frame-182"></div>
+  <img class="test-tubes-and-flask" src={testubes} alt="testubes" />
+  <img class="physics" src={physics} alt="physics" />
+  <div class="physics2">
+    <span>
+      <span class="physics-2-span">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        Physics
+        <br />
+      </span>
+      <span class="physics-2-span2"><br /></span>
+    </span>
+  </div>
+  <img class="maths" src={maths} alt="maths" />
+  <div class="mathematics">
+    <span>
+      <span class="mathematics-span">
+        Mathematics
+        <br />
+      </span>
+      <span class="mathematics-span2"><br /></span>
+    </span>
+  </div>
+  <div class="chemistry">
+    <span>
+      <span class="chemistry-span">
+        <br />
+        <br />
+        <br />
+        <br />
+        Chemistry
+        <br />
+      </span>
+      <span class="chemistry-span2"><br /></span>
+      <span class="chemistry-span3"></span>
+    </span>
+  </div>
+  <img class="svnit" src={svnit} alt="svnit"/>
+ 
+  <div class="fef-32-f">fef32f</div>
+</div>
+
+  );
+};
+
+export default Home;
